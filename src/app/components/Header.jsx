245c8928d7806010782fd00a7ff4ad4672179df0
krishna <kkriss4683@gmail.com>
2025-11-04 03:54:11 +0530

    add header components

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+export default function Header() {
+  return (
+    <div className="flex justify-between items-center p-3 max-w-6xl mx-auto">
+      <ul className="flex gap-4">
+        <li className="hidden sm:block">
+          <Link href={"/sign-in"}>Sign in</Link>
+        </li>
+        <li className="hidden sm:block">
+          <Link href={"/"}>Home</Link>
+        </li>
+        <li className="hidden sm:block">
+          <Link href={"/favorites"}>Favorites</Link>
+        </li>
+        <li className="hidden sm:block">
+          <Link href={"/about"}>About</Link>
+        </li>
+      </ul>
+      <div className="flex items-center gap-4">
+        <Link href={"/"} className="flex gap-1 items-center">
+          <span className="text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg">
+            MOVIE
+          </span>
+          <span className="text-xl hidden sm:inline">rating</span>
+        </Link>
+      </div>
+    </div>
+  );
+}
