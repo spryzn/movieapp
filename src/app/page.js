d567b7b6177f0f3ebc4f65b1f4fa5326eaf25227
krishna <kkriss4683@gmail.com>
2025-11-08 14:46:23 +0530

    Refactor Home and TopPages components to include Results component and enhance data fetching logic; add Card and Results components for better structure and display of movie data.

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,55 @@
-import React from 'react'
+import Results from '@/components/Results';
+const API_KEY = process.env.API_KEY;
+export default async function Home() {
+  const res = await fetch(
+    `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US&page=1`
+  );
+  const data = await res.json();
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+  const results = data.results;
+  let homePageContent = null;
+  try {
+    const results = await fetch(process.env.URL + '/api/homepagecontent/get', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      cache: 'no-store',
+    });
 
-export default function Home() {
+    if (!results.ok) {
+      throw new Error('Failed to fetch data');
+    }
+
+    const text = await results.text();
+    if (text) {
+      const data = JSON.parse(text);
+      homePageContent = data[0] || null;
+    } else {
+      console.log('Empty response');
+    }
+  } catch (error) {
+    console.log('Error getting the home page content', error);
+  }
+
   return (
-    <div className='text-red-600'>
-      Home Page
+    <div>
+      {homePageContent && (
+        <div className='text-center mb-10 max-w-6xl mx-auto py-10'>
+          <h1 className='text-4xl font-extrabold mb-4 bg-gradient-to-r from-purple-600 via-pink-500 to-red-500 bg-clip-text text-transparent'>
+            {homePageContent.title}
+          </h1>
+          <div
+          className='sm:text-lg p-4'
+            dangerouslySetInnerHTML={{ __html: homePageContent.description }}
+          ></div>
+        </div>
+      )}
+      <div>
+        <Results results={results} />
+      </div>
     </div>
-  )
+  );
 }
--- src/app/top/[genre]/page.jsx
+++ src/app/top/[genre]/page.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import Results from '@/components/Results';
+const API_KEY = process.env.API_KEY;
+export default async function Home({ params }) {
+  const { genre } = await params;
+  const res = await fetch(
+    `https://api.themoviedb.org/3${
+      genre === 'rated' ? `/movie/top_rated` : `/trending/all/week`
+    }?api_key=${API_KEY}&language=en-US&page=1`
+  );
+  const data = await res.json();
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+  const results = data.results;
 
-export default function TopPages() {
   return (
     <div>
-      TopPage
+      <Results results={results} />
     </div>
-  )
+  );
 }
